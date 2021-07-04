9f5959c4fdff1034f5bd4ebcdf57e93c4100a17e
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-07-05 02:30:24 +0530

    use delayed pullbacks

--- src/trial.jl
+++ src/trial.jl
@@ -1,18 +1,39 @@
 function dag_chain(c::Chain, ip)
-  outs = []
-  pbs = []
   # y = ip
-  thpb = delayed(Zygote.pullback)((m,x) -> m(x), c[1], ip)
-  thy = delayed(getindex)(thpb, 1)
-
+  pb = delayed(Zygote.pullback)((m,x) -> m(x), c[1], ip)
+  thy = delayed(getindex)(pb, 1)
+  back = delayed(getindex)(pb, 2)
+  # back = delayed((m,x) -> m(x))(back_, thy)
+  backs = [back]
+  ys = [thy]
   for m in c[2:end]
     @show m
     # thy = delayed(getindex)(thpb, 1)
-    thpb = delayed(Zygote.pullback)((m,x) -> m(x), m, thy)
-    thy = delayed(getindex)(thpb, 1)
-    push!(pbs, thpb)
+    pb = delayed(Zygote.pullback)((m,x) -> m(x), m, thy)
+    thy = delayed(getindex)(pb, 1)
+    back_ = delayed(getindex)(pb, 2)
+    # back_(back(y)[2])
+    b = delayed((m,x) -> m(x))(back_, thy)
+    b_ = delayed(getindex)(b, 2)
+    back = delayed((m,x) -> m(x))(back_, b_)
+    push!(backs, back_)
+    push!(ys, thy)
   end
-  thy, thpb
+  ys[end], Δ -> makedag(backs, Δ)
+end
+
+function makedag(backs, Δ)
+  b = delayed((m,x) -> m(x))(backs[end], Δ)
+  out = [b]
+  # reverse the elements of backs except the last one
+  for b_ in backs[end-1:-1:1]
+    g = delayed(getindex)(b, 2)
+    b = delayed((m,x) -> m(x))(b_, g)
+
+    push!(out, b)
+  end
+  cout = collect.(out)
+  ((layers = Tuple(reverse(first.(cout))),)), cout[end][2]
 end
 
 dag_chain(f, x...) = delayed(Zygote.pullback)(f, (x...))
