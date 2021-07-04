9932b4a1464ee5058dc7293b0454f2f741760bb6
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-07-05 00:00:22 +0530

    simpler manual construction

--- src/trial.jl
+++ src/trial.jl
@@ -1,18 +1,41 @@
 function dag_chain(c::Chain, ip)
-  outs = []
-  pbs = []
+  # outs = []
+  # pbs = []
   # y = ip
   thpb = delayed(Zygote.pullback)((m,x) -> m(x), c[1], ip)
-  thy = delayed(getindex)(thpb, 1)
-
+  # thy = delayed(getindex)(thpb, 1)
+  # outs = [thy]
+  # pbs = [thpb]
+  pb = delayed(getindex)(thpb, 2)
+  # backval = delayed((m,x) -> m(x))(pb, thy)
+  thy = delayed(c[1])(ip)
+  y, back1 = Zygote.pullback((m,x) -> m(x), c[1], ip)
+  backs = Any[back1]
+  # pbs = [backval]
   for m in c[2:end]
     @show m
     # thy = delayed(getindex)(thpb, 1)
     thpb = delayed(Zygote.pullback)((m,x) -> m(x), m, thy)
-    thy = delayed(getindex)(thpb, 1)
-    push!(pbs, thpb)
+    y, back = Zygote.pullback((m,x) -> m(x), m, y)
+    # thy = delayed(getindex)(thpb, 1)
+    thy = delayed(m)(thy)
+    pb = delayed(getindex)(thpb, 2)
+    backval = delayed((m,x) -> m(x))(pb, thy)
+    pushfirst!(backs, back)
+    # push!(pbs, backval)
   end
-  thy, thpb
+  thy, Δ -> makedag(backs, Δ) # , pbs # thpb
 end
 
 dag_chain(f, x...) = delayed(Zygote.pullback)(f, (x...))
+
+function makedag(backs, Δ)
+  b = delayed(first(backs))(Δ)
+  out = [b]
+  for b_ in backs[2:end]
+    b = delayed(b_)(delayed(getindex)(b, 2))
+    push!(out, b)
+  end
+  ∂m, ∂x = collect(out[end])
+  ((layers = (∂m, reverse!(first.(collect.(out[begin:end-1])))...),), ∂x)
+end
