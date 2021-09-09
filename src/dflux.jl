2f7f683b7f1e5476bb0b3c67249c9e31d13f6bdc
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-09-09 23:17:28 +0530

    add parallel version

--- src/dflux.jl
+++ src/dflux.jl
@@ -8,7 +8,7 @@ struct DaggerChain
 end
 
 daglayer(f, args...) = delayed((m,x...) -> m(x...))(Dagger.tochunk(f, DaggerGPU.CuArrayDeviceProc(1, CUDA.device().handle, CUDA.uuid(CUDA.device()))), args...)
-# daglayer(par::Parallel, ip...) = delayed((x...) -> par.connection(x...))(daglayer(f, ip...) for f in par.layers)
+daglayer(par::Parallel, ip...) = delayed((x...) -> par.connection(x...))(daglayer(f, ip...) for f in par.layers)
 
 function (dc::DaggerChain)(x)
     t = foldl(dc.chain.layers; init = x) do l1, l2
